b2239491f8b012bbb66a6f00467bd71f37d77502
ismailcanaydin <ismailcanaydin@gmail.com>
2022-10-21 17:48:16 +0300

    Final

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
-import { ExampleComponent } from 'aysoo'
+import { ExampleComponent, Button } from 'aysoo'
 import 'aysoo/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <>
+    <div style={{ textAlign: 'center' }}>
+      <ExampleComponent text="Merhaba 😄" />
+      <Button type="primary" text="Primary Button"></Button>
+      <br></br>
+      <Button type="default" text="Default Button"></Button>
+      <br></br>
+      <Button type="dashed" text="Dashed Button"></Button>
+      <br></br>
+      <Button type="texty" text="Text Button"></Button>
+      <br></br>
+      <Button type="link" text="Link Button"></Button>
+    </div>
+  </>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,3 +4,47 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+
+export const Button = ({ text, type }) => {
+
+  if (type == "primary") {
+    return (
+      <button className={styles.primary}>{text}
+      </button>
+    )
+  }
+  if (type == "default") {
+    return (
+      <button className={styles.default}>{text}
+      </button>
+    )
+  }
+  if (type == "dashed") {
+    return (
+      <button className={styles.dashed}>{text}
+      </button>
+    )
+  }
+  if (type == "texty") {
+    return (
+      <button className={styles.texty}>{text}
+      </button>
+    )
+  }
+  if (type == "link") {
+    return (
+      <button className={styles.link}>{text}
+      </button>
+    )
+  }
+
+}
+
+
+// export const Button = (props) => {
+
+//   return (
+//     <button type='primary' className={styles.primary} {...props}>{props.text}</button>
+//   )
+// }
